ed849cb98a1add0703bcb541dedafcd5380bf93e
lalahaha323 <2665687186@qq.com>
2019-06-03 17:04:42 +0800

    接受请求的输入

---
+++ chapter5/mvc_5.3.2/src/main/java/spittr/web/SpittleController.java
@@ -0,0 +1,34 @@
+package spittr.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import spittr.Spittle;
+import spittr.data.SpittleRepository;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/spittles")
+public class SpittleController {
+    private SpittleRepository spittleRepository;
+
+    @Autowired
+    public SpittleController(SpittleRepository spittleRepository) {
+        this.spittleRepository = spittleRepository;
+    }
+    @RequestMapping(value="/show", method=RequestMethod.GET)
+    public String showSpittle(@RequestParam("spittle_id")long spittleId, Model model) {
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
+    }
+    @RequestMapping(value="/{spittleId}", method=RequestMethod.GET)
+    public String spittle(@PathVariable("spittleId") long spittleId,Model model) {
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
+    }
+}
