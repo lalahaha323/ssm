18f26de452e6c0117b1764c427bc467d4b40f647
lalahaha323 <2665687186@qq.com>
2019-06-03 14:19:36 +0800

    接受请求的输入

---
+++ chapter5/mvc_5.3.1/src/main/java/spittr/config/SpittrWebAppInitializer.java
@@ -0,0 +1,19 @@
+package spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//扩展AbstractAnnotationConfigDispatcherServletInitializer的类都会自动地配置DispatcherServlet和Spring应用上下文
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {RootConfig.class};
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {WebConfig.class};
+    }
+}
---
+++ chapter5/mvc_5.3.1/src/test/java/web/HomeControllerTest.java
@@ -0,0 +1,48 @@
+package web;
+
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
+import spittr.Spittle;
+import spittr.data.SpittleRepository;
+import spittr.web.SpittleController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class HomeControllerTest {
+    @Test
+    public void shouldShowPagedSpittles() throws Exception {
+        List<Spittle> expectedSpittles = createSpittleList(50);
+        SpittleRepository mockRepository = mock(SpittleRepository.class);
+        when(mockRepository.findSpittles(238900, 50))
+                .thenReturn(expectedSpittles);
+
+        SpittleController controller = new SpittleController(mockRepository);
+        MockMvc  mockMvc = standaloneSetup(controller)
+                .setSingleView(new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+                .build();
+        mockMvc.perform(get("/spittles?max=238900&count=50"))
+                .andExpect(view().name("spittles"))
+                .andExpect(model().attributeExists("spittleList"))
+                .andExpect(model().attribute("spittleList", hasItems(expectedSpittles.toArray())));
+    }
+    private List<Spittle> createSpittleList(int count) {
+        List<Spittle> spittles = new ArrayList<Spittle>();
+        for(int i = 0; i < count; i++) {
+            spittles.add(new Spittle("Spittle " + i, new Date()));
+        }
+        return spittles;
+    }
+
+}
