e24ecb83c835c1119beb575ddd579f9bbe1efa0b
lalahaha323 <2665687186@qq.com>
2019-06-03 13:48:49 +0800

    改进优化控制器

--- chapter5/mvc_5.3/src/main/java/spittr/web/SpittleController.java
+++ chapter5/mvc_5.3/src/main/java/spittr/web/SpittleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import spittr.data.SpittleRepository;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
@@ -21,4 +23,14 @@ public class SpittleController {
                 spittleRepository.findSpittles(Long.MAX_VALUE, 20));
         return "spittles";
     }
+//    @RequestMapping(method=RequestMethod.GET)
+//    public String spittles(Model model) {
+//        model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+//        return "spittles";
+//    }
+//    @RequestMapping(method=RequestMethod.GET)
+//    public String spittles(Map model) {
+//        model.put("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+//        return "spittles";
+//    }
 }
